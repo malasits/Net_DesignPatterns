3383b37a6aba5298187f1fb7a125915295055f4b
malasits <malasits&hotmail.com>
2018-10-17 19:49:42 +0200

    initial commit

---
+++ Net_DesignPatterns/NameConventions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net_DesignPatterns
+{
+    class NameConventions
+    {
+        //1. -Használj PascalCase -t osztálynevek és metódusnevek deklarálásához
+        //2. -Használj camelCase -t metódus paraméterek és metódus változók deklarálásához
+        //3. -Ne használj magyar jelölést vagy bármilyen más típusú azonosítást az azonosítókban
+        //4. -Ne használj CAPS-LOCK ot readonly és constans változóknál
+        //5. -Kerüld a rövidítéseket, kivéve => a gyakran használt azonosítók esetén (pl.: Id, Xml, Ftp, Uri)
+        //6. -Használj PascalCase-t 3 vagy több rövídítések esetén (pl.: HtmlHelper, FtpTransfer )
+        //   -  2karakteres rövidítés esetén mindegyiknagybetű (pl.: UIControl )
+    }
+}
---
+++ Net_DesignPatterns/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net_DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-- Design Patterns --");
+            Console.WriteLine("Szabványos kódolási minták és szabályok!");
+            Console.WriteLine("Referencia => https://www.dofactory.com/");
+
+            Console.ReadKey();
+        }
+    }
+}
